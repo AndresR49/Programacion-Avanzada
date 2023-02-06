5946f61ff986cc610c2de3c458ce5183fe123dcb
AndresR49 <a.feliperq@gmail.com>
2023-02-06 15:51:08 -0500

    Update main.cpp

--- Prog1/main.cpp
+++ Prog1/main.cpp
@@ -1,14 +1,127 @@
-//
-//  main.cpp
-//  Prog1
-//
-//  Created by Andres on 6/02/23.
-//
+
+/*
+---------------------------------------------------------------------------------------
+ Fecha: 01/02/2023
+ Autor: Andrés Felipe Ramírez Quintero
+ Problema: Un gimnasio se propone a automatizar el proceso de registro. Se necesita
+ solicitar el numero de usuarios a registrar inicialmente. Se debe solicitar la mayor
+ cantidad de datos posibles, para ser usados posteriormente. De ser necesario, se
+ recomienda preguntar datos adicionales sobre sus hijos (nombre, edad, entre otros,
+ adicional). Se necesita que al final de la ejecución, se presente el nombre de los 10
+ usuarios registrados satisfactoriamente.
+  Tipo de datos
+ Solución:
+ - se hará un TDA general para la captura de datos del usuario.
+ - se hará un TDA anidado, para la captura de datos de los hijos
+ - se presentara todos los anuncios posibles descriptivos
+-----------------------------------------------------------------------------------------
+*/
 
 #include <iostream>
+#include <string>
+using namespace std;
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+//se crea un TDA para los datos de los usuarios que se desean registrar
+struct cliente
+{
+    string nombre;
+    int edad;
+    long long int ID;
+    double MasaCorporal;
+    char genero;
+    string correo;
+    long long int movil;
+    int cHijos;
+    string direccion;
+};
+
+//TDA anidado para captura de datos del hijo
+struct hijo_cliente
+{
+    string nombre;
+    int edad;
+    long long int ID;
+};
+
+
+int main ()
+{
+    int personas;
+    int p;
+    cout<<"********Bienvenido! Espero se encuentre muy bien el día de hoy el dia de hoy?********"<<endl;
+    cout<<"ingrese el numero de usuarios que desea registrar"<<endl;
+    //declaracion de variables y solicitud de cantidad de personas a registrar
+    cin>>personas;
+    //arreglo para guardar la informacion del numero de personas solicitada
+    cliente clientes[personas];
+
+    //for para empezar a guardar la informacion
+    for (int i=0; i<personas; i++)
+    {
+        cout<<i<<"-> Ingrese su nombre"<<endl;
+        cin>>clientes[i].nombre;
+        cout<<i<<"-> Ingrese su edad"<<endl;
+        cin>>clientes[i].edad;
+        cout<<i<<"-> Ingrese su identificacion"<<endl;
+        cin>>clientes[i].ID;
+        cout<<i<<"-> Ingrese su masa corporal"<<endl;
+        cin>>clientes[i].MasaCorporal;
+        cout<<i<<"-> Ingrese su genero (f/m)"<<endl;
+        cin>>clientes[i].genero;
+        cout<<i<<"-> Ingrese su correo"<<endl;
+        cin>>clientes[i].correo;
+        cout<<i<<"-> Ingrese su movil"<<endl;
+        cin>>clientes[i].movil;
+        cout<<i<<"-> Ingrese cantidad de hijos"<<endl;
+        cin>>clientes[i].cHijos;
+        p=clientes[i].cHijos;
+        hijo_cliente hijo[p];
+        if (clientes[i].cHijos!=0)
+
+        {
+            int n=1;
+            for (int k=0; k<p; k++)
+                {
+                    cout<<"ingrese el nombre del hijo "<<n<<endl;
+                    cin>>hijo[k].nombre;
+                    cout<<"ingrese la edad del hijo "<<n<<endl;
+                    cin>>hijo[k].edad;
+                    cout<<"ingrese el ID de hijo "<<n<<endl;
+                    cin>>hijo[k].ID;
+                    n++;
+                }
+        }
+        cout<<"Ingrese su direccion"<<endl;
+        cin>>clientes[i].direccion;
+
+        //imprime la informacion ingresada anteriormente
+        cout<<" la informacion ingresada fue: "<<endl;
+        cout<<"nombre: "<<clientes[i].nombre<<endl;
+        cout<<"edad: "<<clientes[i].edad<<endl;
+        cout<<"identificacion: "<<clientes[i].ID<<endl;
+        cout<<"Masa corporal: "<<clientes[i].MasaCorporal<<"kg"<<endl;
+
+        // imprime femenino o masculino dependiendo de la letra ingresada en genero f/m
+        if (clientes[i].genero== 'f')
+        {
+            cout<<"Genero: Femenino"<<endl;
+        }
+        else if (clientes[i].genero== 'm')
+        {
+            cout<<"Genero: Masculino"<<endl;
+        }
+        cout<<"correo: "<<clientes[i].correo<<endl;
+        cout<<"movil: "<<clientes[i].movil<<endl;
+        cout<<"Cantidad de hijos: "<<clientes[i].cHijos<<endl;
+    }
+
+    cout<<"los usuarios han sido registrados satisfactoriamente"<<endl;
+    cout<<"los nombres de los usuarios registrados son: "<<endl;
+    //imprime el nombre de los usuarios con un for
+    for (int j=0; j<personas; j++)
+    {
+        cout<<clientes[j].nombre<<endl;
+    }
+    cout<<"Fin del programa, Feliz Día. "<<endl;
     return 0;
 }
